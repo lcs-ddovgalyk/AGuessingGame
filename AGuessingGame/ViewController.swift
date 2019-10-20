7473bc7625ff57783c405181eaa165d84f514255
lcs-ddovgalyk <ddovgalyk@lcs.on.ca>
2019-10-20 17:03:55 -0400

    Made the phone talk

--- AGuessingGame/ViewController.swift
+++ AGuessingGame/ViewController.swift
@@ -7,13 +7,28 @@
 //
 
 import UIKit
-class ViewController: UIViewController {
+import AVFoundation
 
+class ViewController: UIViewController {
+    //runs as soon as the view becomes visible to the user
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //Make an object named 'synthesizer', which is an instance of the class 'AVSpeechSynthesizer'
+        let synthesizer = AVSpeechSynthesizer()
+        //make a string that contains what we want the computer to say
+        let message = "I am thinking of a number between 1 and 100. Guess what it is."
+        
+        //make an object named 'utturance', which is an instance of a class AVSpeechUtturance
+        let utturance = AVSpeechUtterance(string: message)
+        
+        //speak the message
+        synthesizer.speak(utturance)
+        
     }
-//a
+    
+
 
 }
 
